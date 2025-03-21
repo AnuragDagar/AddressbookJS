4d75f5a566e518e53d2e1365831810559ff9c588
Anuragdagar <anuragdagar8126@gmail.com>
2025-03-21 22:15:12 +0530

    UC4

--- NumberofContact.js
+++
@@ -1,7 +0,0 @@
-let addressBook = require('./AddContacts');
-function getNumberOfContacts() {
-    return addressBook.reduce((count, contact) => count + 1, 0);
-}
-
-// Example Usage
-console.log("Number of Contacts:", getNumberOfContacts());
